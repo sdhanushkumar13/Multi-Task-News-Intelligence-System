aaefb52cb8e7a1cf756701caa63b75c35a38f50a
Dhanush <sdhanushkumar13@gmail.com>
2026-03-24 20:55:55 +0530

    Update aws.py

--- aws.py
+++ aws.py
@@ -48,8 +48,8 @@ def log_to_db(user_id, task, model_family, model_name, input_length, output, err
         database=DB_NAME,
         user=DB_USER,
         password=DB_PASSWORD,
-        port=5432,
-        connection_timeout=10)
+        port=5432)
+        #connection_timeout=10)
         
         cur = conn.cursor()
         print(f" user:{user_id}, task:{task_type}, modelfam:{model_family}, modelname:{model_name}, inputlength:{input_length}, out:{output}, errorflag:{error_flag} ")
